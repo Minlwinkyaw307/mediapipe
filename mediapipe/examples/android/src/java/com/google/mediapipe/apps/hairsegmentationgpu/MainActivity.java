fc2a99a6dacbea367838076c11165397064ee13c
Sandipan Haldar <h.sandipan05@gmail.com>
2019-08-18 03:44:47 +0530

    added seekbar

--- mediapipe/examples/android/src/java/com/google/mediapipe/apps/hairsegmentationgpu/MainActivity.java
+++ mediapipe/examples/android/src/java/com/google/mediapipe/apps/hairsegmentationgpu/MainActivity.java
@@ -1,27 +1,17 @@
-// Copyright 2019 The MediaPipe Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package com.google.mediapipe.apps.hairsegmentationgpu;
+package com.example.edgedetection;
 
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Size;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.mediapipe.components.CameraHelper;
 import com.google.mediapipe.components.CameraXPreviewHelper;
 import com.google.mediapipe.components.ExternalTextureConverter;
@@ -30,138 +20,218 @@ import com.google.mediapipe.components.PermissionHelper;
 import com.google.mediapipe.framework.AndroidAssetUtil;
 import com.google.mediapipe.glutil.EglManager;
 
-/** Main activity of MediaPipe example apps. */
+import com.google.mediapipe.framework.PacketCreator;
+
+
+
+/**
+ * Main activity of MediaPipe example apps.
+ */
 public class MainActivity extends AppCompatActivity {
-  private static final String TAG = "MainActivity";
+    private static final String TAG = "MainActivity";
 
-  private static final String BINARY_GRAPH_NAME = "hairsegmentationgpu.binarypb";
-  private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
-  private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
-  private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
+    private static final String BINARY_GRAPH_NAME = "hairsegmentationgpu.binarypb";
+    private static final String INPUT_VIDEO_STREAM_NAME = "input_video";
+    private static final String OUTPUT_VIDEO_STREAM_NAME = "output_video";
+    private static final CameraHelper.CameraFacing CAMERA_FACING = CameraHelper.CameraFacing.FRONT;
+    
 
-  // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
-  // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
-  // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
-  // corner, whereas MediaPipe in general assumes the image origin is at top-left.
-  private static final boolean FLIP_FRAMES_VERTICALLY = true;
-
-  static {
-    // Load all native libraries needed by the app.
-    System.loadLibrary("mediapipe_jni");
-    System.loadLibrary("opencv_java4");
-  }
-
-  // {@link SurfaceTexture} where the camera-preview frames can be accessed.
-  private SurfaceTexture previewFrameTexture;
-  // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
-  private SurfaceView previewDisplayView;
-
-  // Creates and manages an {@link EGLContext}.
-  private EglManager eglManager;
-  // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
-  // frames onto a {@link Surface}.
-  private FrameProcessor processor;
-  // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
-  // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
-  private ExternalTextureConverter converter;
-
-  // Handles camera access via the {@link CameraX} Jetpack support library.
-  private CameraXPreviewHelper cameraHelper;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    previewDisplayView = new SurfaceView(this);
-    setupPreviewDisplayView();
-
-    // Initilize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
-    // binary graphs.
-    AndroidAssetUtil.initializeNativeAssetManager(this);
-
-    eglManager = new EglManager(null);
-    processor =
-        new FrameProcessor(
-            this,
-            eglManager.getNativeContext(),
-            BINARY_GRAPH_NAME,
-            INPUT_VIDEO_STREAM_NAME,
-            OUTPUT_VIDEO_STREAM_NAME);
-    processor.getVideoSurfaceOutput().setFlipY(FLIP_FRAMES_VERTICALLY);
-
-    PermissionHelper.checkAndRequestCameraPermissions(this);
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    converter = new ExternalTextureConverter(eglManager.getContext());
-    converter.setFlipY(FLIP_FRAMES_VERTICALLY);
-    converter.setConsumer(processor);
-    if (PermissionHelper.cameraPermissionsGranted(this)) {
-      startCamera();
+    public int red_progress = 0;
+    public int blue_progress = 0;
+    public int green_progress = 0;
+    SeekBar red_seekBar;
+    SeekBar green_seekBar;
+    SeekBar blue_seekBar;
+    static {
+        // Load all native libraries needed by the app.
+        System.loadLibrary("mediapipe_jni");
+        System.loadLibrary("opencv_java4");
     }
-  }
 
-  @Override
-  protected void onPause() {
-    super.onPause();
-    converter.close();
-  }
+    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
+    private SurfaceTexture previewFrameTexture;
+    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
+    private SurfaceView previewDisplayView;
 
-  @Override
-  public void onRequestPermissionsResult(
-      int requestCode, String[] permissions, int[] grantResults) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
-  }
+    // Creates and manages an {@link EGLContext}.
+    private EglManager eglManager;
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    private FrameProcessor processor;
+    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
+    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
+    private ExternalTextureConverter converter;
 
-  private void setupPreviewDisplayView() {
-    previewDisplayView.setVisibility(View.GONE);
-    ViewGroup viewGroup = findViewById(R.id.preview_display_layout);
-    viewGroup.addView(previewDisplayView);
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    private CameraXPreviewHelper cameraHelper;
+    private
 
-    previewDisplayView
-        .getHolder()
-        .addCallback(
-            new SurfaceHolder.Callback() {
-              @Override
-              public void surfaceCreated(SurfaceHolder holder) {
-                processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
-              }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        red_seekBar = (SeekBar) findViewById(R.id.red_seekbar);
+        blue_seekBar = (SeekBar) findViewById(R.id.green_seekbar);
+        green_seekBar = (SeekBar) findViewById(R.id.blue_seekbar);
 
-              @Override
-              public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-                // (Re-)Compute the ideal size of the camera-preview display (the area that the
-                // camera-preview frames get rendered onto, potentially with scaling and rotation)
-                // based on the size of the SurfaceView that contains the display.
-                Size viewSize = new Size(width, height);
-                Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
 
-                // Connect the converter to the camera-preview frames as its input (via
-                // previewFrameTexture), and configure the output width and height as the computed
-                // display size.
-                converter.setSurfaceTextureAndAttachToGLContext(
-                    previewFrameTexture, displaySize.getWidth(), displaySize.getHeight());
-              }
+        red_seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
-              @Override
-              public void surfaceDestroyed(SurfaceHolder holder) {
-                processor.getVideoSurfaceOutput().setSurface(null);
-              }
-            });
-  }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                
+                red_progress = i;
+            }
 
-  private void startCamera() {
-    cameraHelper = new CameraXPreviewHelper();
-    cameraHelper.setOnCameraStartedListener(
-        surfaceTexture -> {
-          previewFrameTexture = surfaceTexture;
-          // Make the display view visible to start showing the preview. This triggers the
-          // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
-          previewDisplayView.setVisibility(View.VISIBLE);
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Seek bar progress is :" + red_progress,
+                        Toast.LENGTH_SHORT).show();
+
+            }
         });
-    cameraHelper.startCamera(this, CAMERA_FACING, /*surfaceTexture=*/ null);
-  }
+
+        green_seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                green_progress = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Seek bar progress is :" + green_progress,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        blue_seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                blue_progress= i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Seek bar progress is :" + blue_progress,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+        previewDisplayView = new SurfaceView(this);
+        setupPreviewDisplayView();
+        // Initilize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(this);
+        packetcreator =new PacketCreator();
+        red_packet = packetcreator.createInt32(red_progress);
+        green_packet = packetcreator.createInt32(green_progress);
+        blue_packet = packetcreator.createInt32(blue_progress);
+        eglManager = new EglManager(null);
+        processor =
+                new FrameProcessor(
+                        this,
+                        eglManager.getNativeContext(),
+                        BINARY_GRAPH_NAME,
+                        INPUT_VIDEO_STREAM_NAME,
+                        OUTPUT_VIDEO_STREAM_NAME);
+
+        PermissionHelper.checkAndRequestCameraPermissions(this);
+
+//         processor.setInputSidePackets("rgb_reference", t);
+//        processor.setInputSidePackets("green_value",green_progress);
+//        processor.setInputSidePackets("blue_value", blue_progress);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        converter = new ExternalTextureConverter(eglManager.getContext());
+        converter.setConsumer(processor);
+        if (PermissionHelper.cameraPermissionsGranted(this)) {
+            startCamera();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        converter.close();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void setupPreviewDisplayView() {
+        previewDisplayView.setVisibility(View.GONE);
+        ViewGroup viewGroup = findViewById(R.id.preview_display_layout);
+        viewGroup.addView(previewDisplayView);
+
+        previewDisplayView
+                .getHolder()
+                .addCallback(
+                        new SurfaceHolder.Callback() {
+                            @Override
+                            public void surfaceCreated(SurfaceHolder holder) {
+                                processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
+                            }
+
+                            @Override
+                            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                                // (Re-)Compute the ideal size of the camera-preview display (the area that the
+                                // camera-preview frames get rendered onto, potentially with scaling and rotation)
+                                // based on the size of the SurfaceView that contains the display.
+                                Size viewSize = new Size(width, height);
+                                Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
+
+                                // Connect the converter to the camera-preview frames as its input (via
+                                // previewFrameTexture), and configure the output width and height as the computed
+                                // display size.
+                                converter.setSurfaceTextureAndAttachToGLContext(
+                                        previewFrameTexture, displaySize.getWidth(), displaySize.getHeight());
+                            }
+
+                            @Override
+                            public void surfaceDestroyed(SurfaceHolder holder) {
+                                processor.getVideoSurfaceOutput().setSurface(null);
+                            }
+                        });
+    }
+
+    private void startCamera() {
+        cameraHelper = new CameraXPreviewHelper();
+        cameraHelper.setOnCameraStartedListener(
+                surfaceTexture -> {
+                    previewFrameTexture = surfaceTexture;
+                    // Make the display view visible to start showing the preview. This triggers the
+                    // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+                    previewDisplayView.setVisibility(View.VISIBLE);
+                });
+        cameraHelper.startCamera(this, CAMERA_FACING, /*surfaceTexture=*/ null);
+    }
 }
+
+
